1cbbb414ebea97f2286a8f5b6410d6670742af48
Igor Forner <igorfornerrodrigues2@gmail.com>
2022-07-21 19:17:39 -0300

    adicionado exercicios 4.1 geral

---
+++ 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercicios/2.js
@@ -0,0 +1,9 @@
+const a = null
+const b = null
+if (a>b){
+    console.log('A é maior do que B')
+}else if (b>a){
+    console.log('B é maior do que A')
+}else{
+    console.log('A e B são iguais')
+}
---
+++ 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercicios/3.js
@@ -0,0 +1,10 @@
+const a = null
+const b = null
+const c = null
+if (a > b && c){
+    console.log('A é o maior dos 3 valores')
+}else if (b > a && c){
+    console.log('B é o maior dos 3 valores')
+}else{
+    console.log('C é o maior dos 3 valores')
+}
