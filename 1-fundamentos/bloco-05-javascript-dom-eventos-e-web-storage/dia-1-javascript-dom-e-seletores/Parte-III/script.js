c78fbbe2d203c1307f443957fa51f8525cb5d97f
Igor Forner <igorfornerrodrigues2@gmail.com>
2022-07-27 18:45:26 -0300

    Adicionado README.md de exercicios e arquivos do "para fixar"

---
+++ 1-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Parte-III/script.js
@@ -0,0 +1,7 @@
+document.getElementById('header-container').style.backgroundColor = '#00B069'
+document.querySelector('.emergency-tasks').style.backgroundColor = '#FF9F84'
+document.querySelectorAll('.emergency-tasks div h3')[0].style.backgroundColor = '#A500F3'
+document.querySelectorAll('.emergency-tasks div h3')[1].style.backgroundColor = '#A500F3'
+document.querySelector('.no-emergency-tasks').style.backgroundColor = '#F9DB5E'
+document.querySelectorAll('.no-emergency-tasks div h3')[0].style.backgroundColor = '#232525'
+document.querySelectorAll('.no-emergency-tasks div h3')[1].style.backgroundColor = '#232525'
